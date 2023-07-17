d1330053990c892adc1832cd9bac02828c50e52b
Thi1503 <thile15032003@gmail.com>
2023-07-17 07:25:44 +0700

    Signed-off-by: Thi1503 <thile15032003@gmail.com>

--- mainindex.c
+++ mainindex.c
@@ -1,101 +1,150 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+// Khai bao ham va cau truc
 
-//Khai báo biến và cấu trúc 
-
-
-//Các cấu trúc của nhân viên 
+// Cau truc cua ngay sinh
+int size;
 struct NgaySinh {
     int ngay;
     int thang;
     int nam;
-}; typedef struct NgaySinh NgaySinh;
+};
+typedef struct NgaySinh NgaySinh;
 
-struct NhanVien{
+// Cau truc cua nhan vien
+struct NhanVien {
     int maSoNV;
     char hoDem[50];
     char tenNV[50];
     char gioiTinh[20];
     NgaySinh ngaySinh;
-}; typedef struct NhanVien NV;
+};
+typedef struct NhanVien NV;
 
-struct NVNode{
-    NV nhanVien;
+// cau truc De tai
+struct DeTai{
+	char maDT[50];
+	char tenDT[100];
+	int namBatDau;
+	int namKetThuc;
+	long int kinhPhi;
+}; 
+typedef struct DeTai DT;
+
+// Cau truc 1 node nhan vien trong danh sach lien ket
+struct NVNode {
+    NV data;
     struct NVNode* next;
-}; typedef struct NVNode NVNode;
+};
+typedef struct NVNode NVNode;
 
-struct NVList{
-    NVNode *head;
-    NVNode *tail ;
-}; typedef struct NVList NVList;
+// cau truc 1 node de tai trong danh sach lien ket
+struct DTNode {
+	DT data;
+	struct DTNode* next;
+};
+typedef struct DTNode DTNode;
+
+// Cau truc cua danh sach lien ket nhan vien
+struct NVList {
+    NVNode* head;
+    NVNode* tail;
+};
+typedef struct NVList NVList;
+
+// cau truc cua danh sach lien ket de tai
+struct DTList{
+	DTNode* head;
+	DTNode* tail;
+};
+typedef struct DTList DTList;
 
 
 
 
 
-// khai báo hàm
-void deleteNewline(char x[]); // hàm xóa xuống dòng
-int countNV(NVList NV_List); // hàm đếm số lượng node trong danh sách nhân viên
-void createNVList(NVList *NV_List); // tạo danh sách nhân viên
-NVNode* createNVNode(NV newNVNode); // tạo một node nhân viên mới
-void importFileNV(NVList *NV_List,int n); // hàm nhập từ file NV.bin
-void exportFileNV(NVList NV_List, int n); // hàm xuất dữ liệu ra file NV.bin
-void enterNgaySinh(NgaySinh* ngay_Sinh);// hàm nhập ngày sinh nhân viên
-void enterNV(NV *newNV); // hàm nhập vào thông tin nhân viên
-void enterNgaySinh(NgaySinh* ngay_Sinh);// hàm nhập ngày sinh nhân viên
-void addHeadNVList(NVList* NV_List, NVNode* node);
-void addTailNVList(NVList* NV_List, NVNode* node);//hàm thêm phần tử vào cuối danh sách nhân viên
-void addMiddleNVListAfterQ(NVList* NV_List, NVNode* p, NVNode* q);// hàm thêm phần tử vào sau phần tử q 
 
-int main(){
-    // NV newNV1,newNV2;
-    // NVNode* newNVNode1;
-    // NVNode* newNVNode2;
-    // int len;
-    // NVList NV_List;
-    // createNVList(&NV_List);
-    // printf("ok");
-    // printf("\nNhap vao thong tinh nhan vien");
-    // printf("\n%10s \t %20s \t %20s \t %10s %s","Ma so nhan vien","Ho ten dem","Ten nhan vien","Gioi tinh", "Ngay sinh");
-    // printf("\n-----------------------------------------------------------------------------------------------------------------------------------");
-    // enterNV(&newNV1);
-    // newNVNode1 = createNVNode(newNV1);
-    // addHeadNVList(&NV_List,&(*newNVNode1));
-    // //printf("\n%d/%d/%d",newNV.ngaySinh.ngay,newNV.ngaySinh.thang,newNV.ngaySinh.nam);
-    // enterNV(&newNV2);
-    // newNVNode2 = createNVNode(newNV2);
-    // addHeadNVList(&NV_List,&(*newNVNode2));
-    // len = countNV(NV_List);
-    // printf("\n%d",len);
-    // exportFileNV(NV_List, len);
-    // importFileNV(&NV_List,len);
-     char fileName[100];
-    printf("\nNhap vao duong dan hoac ten file:");
-    fflush(stdin);
-    fgets(fileName, sizeof(fileName), stdin);
-    deleteNewline(fileName);
 
-    FILE *f;
-    f = fopen(fileName, "wb");
- 
+
+
+
+
+
+
+// Khai bao cac ham cua nhan vien
+void deleteNewline(char x[]);             // xoa xuong dong 
+int countNV(NVList NV_List);               // dem so node
+void createNVList(NVList* NV_List);        // tao danh sach lien ket nhan vien
+NVNode* createNVNode(NV newNVNode);        // tao node nhan vien
+void enterNgaySinh(NgaySinh* ngay_Sinh);   // nhap vap ngay sinh
+void enterNV(NV* newNV);                   // nhap vao thong tin nhan vien
+void addHeadNVList(NVList* NV_List, NVNode* node); // them 1 node vao dau danh sach
+void addTailNVList(NVList* NV_List, NVNode* node);         //them 1 node vao cuoi danh sach  
+void addAtPositionNVList(NVList* NV_List, NVNode* new_node, int position); 
+int removeHeadNVList(NVList* NV_List);  //xoa nhan vien dau danh sach
+int removeTailNVList(NVList* NV_List);
+int removeAtPositionNVList(NVList* NV_List, int position);
+int updateNVList(NVList *NV_List);
+void printNVList(NVList NV_List);     //in ra danh sach nhan vien
+void exportFileNV(NVList NV_List);    // xuat ra file NV.bin
+void importFileNV(NVList* NV_List);   // doc thong tin file NV.bin
+
+// khai bao ham cua de tai
+int countDT(DTList DT_List);
+void createDTList(DTList* DT_List);
+DTNode* createDTNode(DT newDTNode);
+void enterDT(DT* newDT); 
+void addHeadDTList(DTList* DT_List, DTNode* node);
+
+
+
+
+
+
+int main() {
+//	int position;
+//    NV newNV1, newNV2;
+//    NVNode *newNVNode1, *newNVNode2;
+    NVList NV_List;
+    DTList DT_List;
+    DT newDT;
+//    createNVList(&NV_List);
+//	importFileNV(&NV_List);
+//    printf("Nhap vao thong tin nhan vien");
+//    enterNV(&newNV1);
+//    printf("\nNhap vao vi tri: ");
+//    scanf("%d",&position);
+//    newNVNode1 = createNVNode(newNV1);
+//    addAtPositionList(&NV_List, newNVNode1,position);
+
+//    enterNV(&newNV2);
+//    newNVNode2 = createNVNode(newNV2);
+//    addTailNVList(&NV_List, newNVNode2);
+    importFileNV(&NV_List);
+    removeHeadNVList(&NV_List);
+    printNVList(NV_List);
+    enterDT(&newDT);
+//	exportFileNV(NV_List);
+//	printNVList(NV_List);
+    return 0;
 }
 
-//hàm xóa xuống dòng
-void deleteNewline(char x[]){
+// xoa xuong dong
+void deleteNewline(char x[]) {
     size_t len = strlen(x);
-    if(x[len-1]=='\n'){
-        x[len-1]=='\0';
+    if (x[len - 1] == '\n') {
+        x[len - 1] = '\0';
     }
 }
 
-//hàm đếm số lượng node trong danh sách nhân viên
-int countNV(NVList NV_List){
-    int count=0;
-    if (NV_List.head != NULL){
+// dem so nhan vien trong danh sach
+int countNV(NVList NV_List) {
+    int count = 0;
+    if (NV_List.head != NULL) {
         NVNode* node = NV_List.head;
-        while(node != NULL){
+        while (node != NULL) {
             count++;
             node = node->next;
         }
@@ -103,153 +152,294 @@ int countNV(NVList NV_List){
     return count;
 }
 
-// hàm tạo danh sách nhân viên
-void createNVList(NVList *NV_List){
+// tao danh sach nhan vien
+void createNVList(NVList* NV_List) {
     NV_List->head = NULL;
     NV_List->tail = NULL;
 }
 
-// tạo một node nhân viên mới
-NVNode* createNVNode(NV newNV){
+// tao 1 node nhan vien
+NVNode* createNVNode(NV newNV) {
     NVNode* newNVNode = (NVNode*)malloc(sizeof(NVNode));
-    newNVNode->nhanVien.maSoNV = newNV.maSoNV;
-    newNVNode->nhanVien.hoDem[50] = newNV.hoDem[50];
-    newNVNode->nhanVien.tenNV[50] = newNV.tenNV[50];
-    newNVNode->nhanVien.gioiTinh[20] = newNV.gioiTinh[20];
-    newNVNode->nhanVien.ngaySinh.ngay = newNV.ngaySinh.ngay;
-    newNVNode->nhanVien.ngaySinh.thang = newNV.ngaySinh.thang;
-    newNVNode->nhanVien.ngaySinh.nam = newNV.ngaySinh.nam;
-    newNVNode->next=NULL;
+    newNVNode->data = newNV;
+    newNVNode->next = NULL;
     return newNVNode;
 }
 
-// hàm nhập dữ liệu từ file
-void importFileNV(NVList *NV_List,int n){
-    char fileName[100];
-    NV newNV;
-    NVNode* node;
-    printf("\nNhap vao duong dan hoac ten file: ");
-    fflush(stdin);
-    fgets(fileName, sizeof(fileName), stdin);
-    deleteNewline(fileName);
-
-    FILE *f;
-    f = fopen(fileName, "rb");
-    if(f==NULL){
-        printf("\nKhong the tim thay file!");
-        printf("\nVui long nhap lai duong dan hoac ten file:");
-        importFileNV(&NV_List, n);
-    }
-    fread(&n, sizeof(n),1,f);
-    for(int i=0; i<n;i++){
-        fread(&newNV, sizeof(NV), 1, f);
-        node = createNVNode(newNV);
-        if(NV_List->head == NULL){
-            addHeadNVList(&(*NV_List), &(*node));
-        }
-        else{
-            addTailNVList(&(*NV_List), &(*node));
-        }
-    }
-    fclose(f);
+// nhap vao ngay sinh 
+void enterNgaySinh(NgaySinh* ngay_Sinh) {
+    printf("\nNgay sinh: ");
+    scanf("%d", &ngay_Sinh->ngay);
+    printf("Thang sinh: ");
+    scanf("%d", &ngay_Sinh->thang);
+    printf("Nam sinh: ");
+    scanf("%d", &ngay_Sinh->nam);
 }
 
-// hàm xuất dữ liệu ra file 
-void exportFileNV(NVList NV_List, int n){
-    char fileName[100];
-    printf("\nNhap vao duong dan hoac ten file:");
-    fflush(stdin);
-    fgets(fileName, sizeof(fileName), stdin);
-    deleteNewline(fileName);
-
-    FILE *f;
-    f = fopen(fileName, "wb");
-    // if(f!=NULL){
-    //     printf("\nKhong tim thay duong dan hoac ten file!");
-    //     printf("\nVui long nhap lai duong dan hoac ten file!");
-    //     exportFileNV(NV_List,n);
-    // }
-    // fwrite(&n, sizeof(n),1,f);
-    // for(int i; i<=0; i++){
-    //     fwrite(&(NV_List.head), sizeof(NVNode),1,f);
-    //     NV_List.head = NV_List.head->next;
-    // }
-    // fclose(f);
-}
-
-// hàm nhập vào ngày sinh nhân viên
-void enterNgaySinh(NgaySinh* ngay_Sinh){
-    printf("\nNgay sinh:");
-    scanf("%d",&ngay_Sinh->ngay);
-    printf("\nThang sinh:");
-    scanf("%d",&ngay_Sinh->thang);
-    printf("\nNam sinh:");
-    scanf("%d",&ngay_Sinh->nam);
-}
-
-
-
-//hàm nhập vào thông tin nhân viên
-void enterNV(NV *newNV){
-    NgaySinh ngay_Sinh;
+// nhap vao thong tin nhan vien
+void enterNV(NV* newNV) {
     printf("\nMa so nhan vien: ");
     scanf("%d", &newNV->maSoNV);
-    printf("\nHo ten dem: ");
-    fflush(stdin);
+    printf("Ho ten dem: ");
+    getchar();
     fgets(newNV->hoDem, sizeof(newNV->hoDem), stdin);
     deleteNewline(newNV->hoDem);
-    printf("\nTen nhan vien:");
-    fflush(stdin);
+    printf("Ten nhan vien: ");
     fgets(newNV->tenNV, sizeof(newNV->tenNV), stdin);
     deleteNewline(newNV->tenNV);
-    printf("\nGioi tinh: ");
-    fflush(stdin);
+    printf("Gioi tinh: ");
     fgets(newNV->gioiTinh, sizeof(newNV->gioiTinh), stdin);
     deleteNewline(newNV->gioiTinh);
-    enterNgaySinh(&ngay_Sinh);
-    newNV->ngaySinh.ngay = ngay_Sinh.ngay;
-    newNV->ngaySinh.thang = ngay_Sinh.thang;
-    newNV->ngaySinh.nam = ngay_Sinh.nam;
+    enterNgaySinh(&newNV->ngaySinh);
 }
 
-// hàm thêm phần tử vào đầu danh sách nhân viên
-void addHeadNVList(NVList* NV_List, NVNode* node){
-    if(NV_List->head == NULL){
+// them 1 node vao dau danh sach
+void addHeadNVList(NVList* NV_List, NVNode* node) {
+    if (NV_List->head == NULL) {
         NV_List->head = node;
         NV_List->tail = node;
-    }
-    else{
+    } else {
         node->next = NV_List->head;
-         NV_List->head = node;
+        NV_List->head = node;
     }
 }
 
-
-//hàm thêm phần tử vào cuối danh sách nhân viên
-void addTailNVList(NVList* NV_List, NVNode* node){
-     if(NV_List->head == NULL){
+// them 1 node vao cuoi danh sach
+void addTailNVList(NVList* NV_List, NVNode* node) {
+    if (NV_List->head == NULL) {
         NV_List->head = node;
         NV_List->tail = node;
-    }
-    else{
+    } else {
         NV_List->tail->next = node;
         NV_List->tail = node;
     }
 }
 
-// hàm thêm phần tử vào sau phần tử q 
-void addMiddleNVListAfterQ(NVList* NV_List, NVNode* p, NVNode* q){
-    if(q!=NULL){
-        p->next =q ->next;
-        q->next = p;
-        if(NV_List->tail==q){
-            NV_List->tail = p;
+void addAtPositionList(NVList* NV_List, NVNode* new_node, int position) {
+    if (position <= 0) {
+        addHeadNVList(NV_List, new_node);
+    } else if (position >= countNV(*NV_List)) {
+        addTailNVList(NV_List, new_node);
+    } else {
+        NVNode* node = NV_List->head;
+        int count = 0;
+        while (count < position - 1) {
+            node = node->next;
+            count++;
         }
-    }
-    else{
-        addHeadNVList(NV_List,p);
+        new_node->next = node->next;
+        node->next = new_node;
     }
 }
+
+int removeHeadNVList(NVList* NV_List){
+	if (NV_List->head != NULL){
+		NVNode* node = NV_List->head;
+		NV_List->head = node->next;
+		free(node);
+		if(NV_List == NULL){
+			NV_List->tail = NULL;
+		}
+		return 1;
+	}
+	return 0;
+}
+
+
+int removeTailNVList(NVList* NV_List) {
+    if (NV_List->head == NULL) {
+    } else if (NV_List->head == NV_List->tail) {
+        free(NV_List->head);
+        NV_List->head = NULL;
+        NV_List->tail = NULL;
+        return 1;
+    } else {
+        NVNode* prev_node = NULL;
+        NVNode* current_node = NV_List->head;
+        while (current_node->next != NULL) {
+            prev_node = current_node;
+            current_node = current_node->next;
+        }
+        free(current_node);
+        prev_node->next = NULL;
+        NV_List->tail = prev_node;
+        return 1;
+    }
+}
+
+
+int removeAtPositionNVList(NVList* NV_List, int position) {
+    if (position <= 0) {
+        return removeHeadNVList(NV_List);
+    } else if (position >= countNV(*NV_List)) {
+        return removeTailNVList(NV_List);
+    } else {
+        NVNode* prev_node = NULL;
+        NVNode* current_node = NV_List->head;
+        int count = 0;
+        while (count < position) {
+            prev_node = current_node;
+            current_node = current_node->next;
+            count++;
+        }
+        prev_node->next = current_node->next;
+        free(current_node);
+        return 1;
+    }
+}
+
+
+
+int updateNVList(NVList* NV_List) {
+	int maSoNV;
+	printf("\nNhap vao ma so nhan vien: ");
+	scanf("%d", &maSoNV);
+    NVNode* node = NV_List->head;
+    while (node != NULL) {
+        if (node->data.maSoNV == maSoNV) {
+            // T�m th?y nh�n vi�n c?n c?p nh?t
+            printf("Nhap thong tin moi cho nhan vien (Ma so nhan vien: %d):\n", maSoNV);
+            enterNV(&node->data);
+            return 1;
+        }
+        node = node->next;
+    }
+    // Kh�ng t�m th?y nh�n vi�n
+    return 0;
+}
+
+
+// in ra danh sach nhan vien
+void printNVList(NVList NV_List) {
+    if (NV_List.head != NULL) {
+        NVNode* node = NV_List.head;
+        printf("\n |%20s \t |%30s \t |%30s \t |%20s \t |\t%s |", "Ma so nhan vien", "Ho ten dem", "Ten nhan vien", "Gioi tinh", "Ngay sinh");
+        while (node != NULL) {
+            printf("\n |%20d \t |%30s \t |%30s \t |%20s \t |\t%02d/%02d/%d|", node->data.maSoNV, node->data.hoDem,
+                node->data.tenNV, node->data.gioiTinh, node->data.ngaySinh.ngay, node->data.ngaySinh.thang, node->data.ngaySinh.nam);
+            node = node->next;
+        }
+    }
+}
+
+// xuat vao file NV.bin
+void exportFileNV(NVList NV_List) {
+    FILE* f;
+    f = fopen("NV.bin", "wb");
+    if (f == NULL) {
+        printf("Khong the mo file de ghi!");
+        return;
+    }
+    int count = countNV(NV_List);
+    fwrite(&count, sizeof(count), 1, f);
+    NVNode* node = NV_List.head;
+    while (node != NULL) {
+        fwrite(&node->data, sizeof(NV), 1, f);
+        node = node->next;
+    }
+    fclose(f);
+}
+
+// doc du lieu tu file NV.bin
+void importFileNV(NVList* NV_List) {
+	int i;
+    FILE* f;
+    f = fopen("NV.bin", "rb");
+    if (f == NULL) {
+        printf("Khong the mo file de doc!");
+        return;
+    }
+    int count;
+    fread(&count, sizeof(count), 1, f);
+    for (i = 0; i < count; i++) {
+        NV newNV;
+        fread(&newNV, sizeof(NV), 1, f);
+        NVNode* newNVNode = createNVNode(newNV);
+        addTailNVList(NV_List, newNVNode);
+    }
+    fclose(f);
+}
+
+
+
+
+
+int countDT(DTList DT_List) {
+    int count = 0;
+    if (DT_List.head != NULL) {
+        DTNode* node = DT_List.head;
+        while (node != NULL) {
+            count++;
+            node = node->next;
+        }
+    }
+    return count;
+}
+
+
+void createDTList(DTList* DT_List) {
+    DT_List->head = NULL;
+    DT_List->tail = NULL;
+}
+
+
+DTNode* createDTNode(DT newDT) {
+    DTNode* newDTNode = (DTNode*)malloc(sizeof(DTNode));
+    newDTNode->data = newDT;
+    newDTNode->next = NULL;
+    return newDTNode;
+}
+
+
+void enterDT(DT* newDT) {
+    printf("\nMa de tai: ");
+    fgets(newDT->maDT, sizeof(newDT->maDT),stdin);
+    deleteNewline(newDT->tenDT);
+    printf("\nTen De Tai: ");
+    fgets(newDT->tenDT, sizeof(newDT->tenDT), stdin);
+    deleteNewline(newDT->tenDT);
+    printf("\nNam bat dau: ");
+    scanf("%d",&newDT->namBatDau);
+    printf("\nNam ket thuc: ");
+    scanf("%d", &newDT->namKetThuc);
+    printf("\nKinh phi de tai: ");
+    scanf("%ld", &newDT->kinhPhi);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
